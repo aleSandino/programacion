e11e7a1463d363cfdcb98608f1d843f051a478fa
Alejandra Sandino <macbook@Alejandras-MacBook-Pro.local>
2017-03-16 18:40:49 +0100

    inicial

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<meta charset="utf-8"/>
+		<meta name="" content="">
+        <link rel="stylesheet" media="screen" href="css/style.css"/>
+        <!--<link rel="shortcut icon" href="/favicon.ico" />-->
+        <meta name="author" content=""/>
+        <!--<meta name="date" content=""/>-->
+        <!--<meta name="description" content=""/>-->
+    </head>
+    <body>
+       
+    </body>
+</html>
